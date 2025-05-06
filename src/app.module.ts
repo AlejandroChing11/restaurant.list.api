44af2f0bb1059747f61e88fb575f0604939a13ba
AlejandroChing11 <alejandroching2004@hotmail.com>
2025-05-06 10:19:11 -0500

    feat: Setting up DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { User } from './auth/entities/user.entity';
+
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432, // TODO: usar variables de entorno
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+
+    AuthModule
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
